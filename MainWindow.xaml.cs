8f77770de6e012fc28a377441b10c4205152129f
susmitha sundar <susmitha.sundar@syncfusion.com>
2024-07-31 18:14:08 +0530

    Include the Demo application.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using SmartComponents.LocalEmbeddings;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SemanticSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double VectorValue = 0.55;
+
+        readonly static LocalEmbedder Embedder = new LocalEmbedder(caseSensitive: false);
+        public static Dictionary<int, EmbeddingF32> ProductEmbeddings { get; set; } = new Dictionary<int, EmbeddingF32>();
+
+        EmbeddingF32 queryVector;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Set ItemsSource for DataGrid
+            ProductInfoViewModel viewModel = new ProductInfoViewModel();
+            grid.ItemsSource = viewModel.ProductDetails; 
+
+            foreach (var expense in viewModel.ProductDetails)
+            {
+                ProductEmbeddings.TryAdd(expense.ProductID, Embedder.Embed($"{expense.ProductName}"));
+            }
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                if (string.IsNullOrEmpty(searchtextBox.Text))
+                {
+                    grid.View.Filter = null;
+                    grid.View.RefreshFilter();
+                    return;
+                }
+                queryVector = Embedder.Embed(searchtextBox.Text);
+                grid.View.Filter = FilterRecords;
+                grid.View.RefreshFilter();
+            }
+        }
+        
+        public bool FilterRecords(object o)
+        {   
+            var item = o as ProductInfo;
+            if (SmartFilterRenderer.Similarity(ProductEmbeddings[item.ProductID], queryVector ) > VectorValue)
+                return true;
+           
+            return false;
+        }
+
+        private void vectortextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                VectorValue = Convert.ToDouble((string)vectortextBox.Text);
+                grid.View.Filter = FilterRecords;
+                grid.View.RefreshFilter();
+            }
+        }
+    }
+  
+    public static class SmartFilterRenderer
+    {
+        public static float Similarity<TEmbedding>(TEmbedding a, TEmbedding b) where TEmbedding : IEmbedding<TEmbedding>
+        {
+            return a.Similarity(b);
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,95 @@
+﻿using Syncfusion.Windows.Shared;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemanticSearch
+{
+    public class ProductInfo : NotificationObject
+    {
+        private int _ProductID;        
+
+        private string _ProductName;
+
+        private bool _Availability;
+
+        private double _Price;
+
+        private int _shippingDays;
+
+
+        [Display(Name = "Product ID")]
+        public int ProductID
+        {
+            get
+            {
+                return this._ProductID;
+            }
+            set
+            {
+                this._ProductID = value;
+                this.RaisePropertyChanged("ProductID");
+            }
+        }
+
+        [Display(Name ="Product Name")]
+        public string ProductName
+        {
+            get
+            {
+                return this._ProductName;
+            }
+            set
+            {
+                this._ProductName = value;
+                this.RaisePropertyChanged("ProductName");
+            }
+        }
+
+        [Display(Name = "Availability")]
+        public bool Availability
+        {
+            get
+            {
+                return this._Availability;
+            }
+            set
+            {
+                this._Availability = value;
+                this.RaisePropertyChanged("Availability");
+            }
+        }
+
+        [Display( Name = "Price")]
+        public double Price
+        {
+            get
+            {
+                return this._Price;
+            }
+            set
+            {
+                this._Price = value;
+                this.RaisePropertyChanged("Price");
+            }
+        }
+
+        [Display(Name = "Shipping Days")]
+        public int ShippingDays
+        {
+            get
+            {
+                return this._shippingDays;
+            }
+            set
+            {
+                this._shippingDays = value;
+                this.RaisePropertyChanged("ShippingDays");
+            }
+        }
+      
+    }
+}
